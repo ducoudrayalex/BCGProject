4d634a45f09edb2050e89d63ce1ad429ccef8d22
Ducoudray <ducoudray.alex@gmail.com>
2015-11-19 11:45:22 +0100

    Initialisation du projet

---
+++ src/bcgproject/BCGProject.java
@@ -0,0 +1,15 @@
+package bcgproject;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class BCGProject {
+    public static void main(String[] args) {
+        JFrame fenetre=new JFrame();
+        fenetre.setTitle("BCGProject");
+        fenetre.setSize(800, 600);
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setVisible(true);
+    }
+    
+}
